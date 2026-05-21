b5c3cd823461cdac3c02b2573b42a24ad8cf71bc
ozkey <ozkey@ozkey.com>
2026-05-21 20:11:34 +1000

    css

--- packages/ui/webpack.config.mjs
+++ packages/ui/webpack.config.mjs
@@ -17,6 +17,10 @@ export default {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
   },
   plugins: [
