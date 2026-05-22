bc806f7100e64eabad5ca173e37fb8febe2bc83b
ozkey <ozkey@ozkey.com>
2026-05-22 23:13:40 +1000

    yarn upgrade

---
+++ packages/ui/src/components/BibleText.tsx
@@ -0,0 +1,30 @@
+import {Box, Card, CardContent, Typography} from "@mui/material";
+import React from "react";
+
+export const BibleText: React.FC = () => {
+    return(
+    <Box key={1}>
+        <Card
+            sx={{
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+                boxShadow: 3,
+                "&:hover": {
+                    boxShadow: 6,
+                    transform: "translateY(-4px)",
+                    transition: "all 0.3s ease",
+                },
+            }}
+        >
+            <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                    1
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    This is a simple card component
+                </Typography>
+            </CardContent>
+        </Card>
+    </Box>)
+}
--- packages/ui/src/components/main.tsx
+++ packages/ui/src/components/main.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Container, Box, Card, CardContent, Typography } from "@mui/material";
+import { Container, Box } from "@mui/material";
+import {BibleText} from "./BibleText";
+import {StudyPanel} from "./StudyPanel";
 
 export const MainPanel = () => {
   return (
@@ -16,32 +18,9 @@ export const MainPanel = () => {
             gap: 3,
           }}
         >
-          {["Col 1", "Col 2"].map((title, index) => (
-            <Box key={index}>
-              <Card
-                sx={{
-                  height: "100%",
-                  display: "flex",
-                  flexDirection: "column",
-                  boxShadow: 3,
-                  "&:hover": {
-                    boxShadow: 6,
-                    transform: "translateY(-4px)",
-                    transition: "all 0.3s ease",
-                  },
-                }}
-              >
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="div">
-                    {title}
-                  </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    This is a simple card component in {title.toLowerCase()}.
-                  </Typography>
-                </CardContent>
-              </Card>
-            </Box>
-          ))}
+            <BibleText />
+            <StudyPanel />
+
         </Box>
       </Container>
     </Box>
