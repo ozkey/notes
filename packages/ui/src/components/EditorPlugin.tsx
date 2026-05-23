f8f6141b299c12f5845a75099d28a2351f943650
ozkey <ozkey@ozkey.com>
2026-05-23 16:11:12 +1000

    editor

--- packages/ui/src/components/EditorPlugin.tsx
+++ packages/ui/src/components/EditorPlugin.tsx
@@ -1,19 +1,66 @@
+import { PluginCommand } from 'suneditor/src/interfaces/index.js';
+import { dom } from 'suneditor/src/helper/index.js';
+import CoreKernel from 'suneditor/src/core/kernel/coreKernel.js';
 
-import { PluginCommand } from 'suneditor/src/interfaces/plugins.js';
-import type { SunEditor } from 'suneditor/types';
 
+// see
+// https://github.com/JiHong88/suneditor/blob/master/src/plugins/command/list_bulleted.js
+
+
+
+
+/**
+ * @class
+ * @description Blockquote plugin
+ */
 class HelloWorld extends PluginCommand {
     static key = 'helloWorld';
+    static className = '';
+    private quoteTag: any;
 
-    constructor(kernel: SunEditor.Kernel) {
+    /**
+     * @constructor
+     * @param {SunEditor.Kernel} kernel - The Kernel instance
+     */
+    constructor(kernel: CoreKernel) {
         super(kernel);
-        this.title = 'Hello World';
+        // plugin basic properties
+        this.title = this.$.lang.tag_blockquote;
         this.icon = '<span style="font-size:14px">HW</span>';
+        // members
+        const el = dom.utils.createElement('div')
+        el.className = 'helloWorld';
+        this.quoteTag = el
+
     }
 
-    action(): void {
-        this.$.html.insert('<p>Hello, World!</p>');
-        this.$.history.push(false);
+    /**
+     * @hook Editor.EventManager
+     * @type {SunEditor.Hook.Event.Active}
+     */
+    active(element: { nodeName: string; }, target: Node | SunEditor.NodeCollection) {
+        if (/helloWorld/i.test(element?.nodeName)) {
+            dom.utils.addClass(target, 'active');
+            return true;
+        }
+
+        dom.utils.removeClass(target, 'active');
+        return false;
+    }
+
+    /**
+     * @override
+     * @type {PluginCommand['action']}
+     */
+    action() {
+        const currentBlockquote = dom.query.getParentElement(this.$.selection.getNode(), '.helloWorld');
+
+        if (currentBlockquote) {
+            // @ts-ignore
+            this.$.format.removeBlock(currentBlockquote, { selectedFormats: null, newBlockElement: null, shouldDelete: false, skipHistory: false });
+        } else {
+            this.$.format.applyBlock(this.quoteTag.cloneNode(true));
+        }
     }
 }
 
